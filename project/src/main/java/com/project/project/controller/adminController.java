bfebc87844c81bc0b485148cab4db76d894cf6ea
ahmed9949 <aosma9949@gamil.com>
2024-03-14 15:36:52 +0200

    test

---
+++ project/src/main/java/com/project/project/controller/adminController.java
@@ -0,0 +1,12 @@
+package com.project.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class adminController {
+     @GetMapping("/home")
+    public String home() {
+        return "adminDb"; // Return the name of your home HTML file without extension
+    }
+}
