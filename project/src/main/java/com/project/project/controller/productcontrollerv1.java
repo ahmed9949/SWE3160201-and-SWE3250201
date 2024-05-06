25fda062b3017c079c7d3f6d024c1f6fa967fcff
Ahmed21gamal <125803560+Ahmed21gamal@users.noreply.github.com>
2024-05-06 23:23:17 +0300

    edit in decrement in quantity

--- project/src/main/java/com/project/project/controller/productcontrollerv1.java
+++ project/src/main/java/com/project/project/controller/productcontrollerv1.java
@@ -213,15 +213,17 @@ public ModelAndView addItem(@Valid @ModelAttribute("Cart") Cart cartItem,
         return mav;
     }
 
-    product.setQuantity(product.getQuantity() - 1);
-    productRepo.save(product); // Save the updated product quantity
-
     // Check if the item already exists for the user
+
     boolean itemExists = cartservic.doesItemExistForUser(userId, productId);
     if (itemExists) {
         ModelAndView mav = new ModelAndView("redirect:/admin/products/product-details/" + productId);
         mav.addObject("message", "Item already exists in the cart");
         return mav;
+    } else {
+        // Decrement the quantity of the product if it's not already in the cart
+        product.setQuantity(product.getQuantity() - 1);
+        productRepo.save(product); // Save the updated product quantity
     }
 
     // Set the User and Product for the Cart item
